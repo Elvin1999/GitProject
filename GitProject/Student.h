bc7b38ea28b75a3c14459f0c4bfdd606f1db7819
Elvin <camalzade_elvin@mail.ru>
2021-06-04 10:37:07 +0400

    i add getter

--- GitProject/Student.h
+++ GitProject/Student.h
@@ -3,9 +3,12 @@
 using namespace std;
 
 class Student {
-public:
 	string name;
+public:
 	void show() {
 		cout << "Name : " << name << endl;
+	}
+	string getName() const {
+		return name;
 	}
 };
